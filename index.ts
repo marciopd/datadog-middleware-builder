9d3d592deea99ae0f4c2b49128caec3c3d77014c
Marcio Paixao Dantas <marciopd@gmail.com>
2019-06-06 14:17:10 +0200

    Exporting measure observable.

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from './src/expressjs/DatadogMiddlewareBuilder';
 export * from './src/expressjs/StatsdSingletonFactory';
 export * from './src/measurer/MeasurePromise';
+export * from './src/measurer/MeasureObservable';
