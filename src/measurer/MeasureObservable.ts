75c372cf48d995bcaea9213562bfa36e4c026a12
Marcio Paixao Dantas <marciopd@gmail.com>
2019-06-06 14:44:45 +0200

    Changing rx-js version.

--- src/measurer/MeasureObservable.ts
+++ src/measurer/MeasureObservable.ts
@@ -4,9 +4,10 @@ import {sendErrorResponseMetrics, sendOkResponseMetrics} from './MeasureCommons'
 
 export const measureObservable = <T>(metricName: string, obs: Observable<T>): Observable<T> => {
     const startTime = Date.now();
-    return obs.pipe(
+    obs.pipe(
         tap(() => sendOkResponseMetrics(metricName, Date.now() - startTime),
             (error) => sendErrorResponseMetrics(metricName, Date.now() - startTime, error),
         ),
     );
+    return obs;
 };
